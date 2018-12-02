6e38180b4e00285769255206458b144181886910
Ilanchezhian Rajamanickam <irajaman@ucsc.edu>
2018-12-01 17:11:06 -0800

    Changed timer to countdown timer

--- app/src/main/java/com/example/trina/equilibriumapp/timer.java
+++ app/src/main/java/com/example/trina/equilibriumapp/timer.java
@@ -1,66 +1,101 @@
 package com.example.trina.equilibriumapp;
-
-import android.os.Bundle;
-import android.os.SystemClock;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.text.Editable;
 
 public class timer extends AppCompatActivity {
-
-    Chronometer simpleChronometer;
-    Button start, stop, restart, setFormat, clearFormat;
-    boolean paused;
+    ProgressBar progressBar;
+    int total;
+    Button start_timer,stop_timer;
+    MyCountDownTimer myCountDownTimer;
+    String seconds, minutes, hours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer);
-        paused = false;
-        // initiate views
-        simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer);
-        simpleChronometer.setTextSize(50);
-        start = (Button) findViewById(R.id.startButton);
-        stop = (Button) findViewById(R.id.stopButton);
-        restart = (Button) findViewById(R.id.restartButton);
-        // perform click  event on start button to start a chronometer
-        start.setOnClickListener(new View.OnClickListener() {
+        start_timer=(Button)findViewById(R.id.button);
+        stop_timer=(Button)findViewById(R.id.button2);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
 
+
+        start_timer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
+                seconds = ((TextView) findViewById(R.id.enterSeconds)).getText().toString();
+                if(seconds == null || seconds.trim().equals("")){
+                    seconds = "00";
+                }
+                minutes = ((TextView) findViewById(R.id.enterMinutes)).getText().toString();
+                if(minutes == null || minutes.trim().equals("")){
+                    minutes = "00";
+                }
+                hours = ((TextView) findViewById(R.id.enterHours)).getText().toString();
+                if(hours == null || hours.trim().equals("")){
+                    hours = "00";
+                }
 
-                simpleChronometer.start();
-            }
-        });
-
-        // perform click  event on stop button to stop the chronometer
-        stop.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                simpleChronometer.stop();
-            }
-        });
-
-        // perform click  event on restart button to set the base time on chronometer
-        restart.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                simpleChronometer.setBase(SystemClock.elapsedRealtime());
-                simpleChronometer.start();
+                total = Integer.parseInt(seconds) + Integer.parseInt(minutes)*60 + Integer.parseInt(hours)*3600;
+                myCountDownTimer = new MyCountDownTimer(total*1000, 1000);
+                myCountDownTimer.start();
 
             }
         });
 
+        stop_timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myCountDownTimer.cancel();
+
+            }
+        });
 
     }
 
+    public class MyCountDownTimer extends CountDownTimer {
 
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            int progress = (int) (millisUntilFinished/1000);
+            int percent = (int)(((float)progress/(float)total)*100);
+            Log.d("wtf",Integer.toString(percent));
+
+            progressBar.setProgress(percent);
+
+            int secondsLeft = progress%60;
+
+
+
+            int minutesLeft = (progress/60)%60;
+
+            int hoursLeft = (progress/3600);
+
+            TextView viewMinutes = (TextView)findViewById(R.id.viewMinutes);
+            TextView viewSeconds = (TextView)findViewById(R.id.viewSeconds);
+            TextView viewHours = (TextView)findViewById(R.id.viewHours);
+            viewMinutes.setText(Integer.toString(minutesLeft));
+            viewSeconds.setText(Integer.toString(secondsLeft));
+            viewHours.setText(Integer.toString(hoursLeft));
+
+        }
+
+        @Override
+        public void onFinish() {
+            finish();
+        }
+    }
 }
